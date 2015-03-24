e81308872d4b7e7c11d4611643985ad9cf9bd4bf
Lewis Nyman <hello@lewisnyman.co.uk>
2015-03-24 16:23:12 +0000

    Update HTML to apply wunderfront gem styling

--- templates/wunderhub_client_team_member.tpl.php
+++ templates/wunderhub_client_team_member.tpl.php
@@ -1,11 +1,15 @@
-<div class="team-member" data-ng-controller="WHTeamMemberController">
-  <div>
-    <img ng-src="{{ TeamMember.picture }}">
-    <div>
+<div class="person person--page" data-ng-controller="WHTeamMemberController">
+  <div class="person__secondary">
+    <h1 class="person__name">
       <span data-ng-bind="TeamMember.firstName"></span>
       <span data-ng-bind="TeamMember.lastName"></span>
-    </div>
+    </h1>
     <div data-ng-bind="TeamMember.jobTitle"></div>
     <div data-ng-bind="TeamMember.office"></div>
+  </div>
+  <div class="person__primary">
+    <div class="person__picture">
+      <img ng-src="{{ TeamMember.picture }}">
+    </div>
   </div>
 </div>
